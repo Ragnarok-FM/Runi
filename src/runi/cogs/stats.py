eedb4208e232256f444c584f7ee3f50fbeca24cb
Tymon Simonides <t.simonides@live.nl>
2026-04-30 18:31:26 +0200

    Remove prefix commands

--- src/runi/cogs/stats.py
+++ src/runi/cogs/stats.py
@@ -12,17 +12,10 @@ class Stats(commands.Cog):
     def __init__(self, bot: 'RuniClient'):
         self.bot = bot
 
-    async def _send_maxsubstats(self, send):
-        embed = self.bot.embed_renderer.render("max_substats", {})
-        await send(embed=embed)
-
     @app_commands.command(name="maxsubstats", description="Show the maximum values for each substat")
-    async def maxsubstats_slash(self, interaction: discord.Interaction):
-        await self._send_maxsubstats(interaction.response.send_message)
-
-    @commands.command(name="maxsubstats")
-    async def maxsubstats_prefix(self, ctx: commands.Context):
-        await self._send_maxsubstats(ctx.send)
+    async def maxsubstats(self, interaction: discord.Interaction):
+        embed = self.bot.embed_renderer.render("max_substats", {})
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: 'RuniClient'):
